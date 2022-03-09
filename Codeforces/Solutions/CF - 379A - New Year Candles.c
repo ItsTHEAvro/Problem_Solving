cfddf6db7abffbcbe6c9af876c4b7d80b3fdf51b
Avro <jyotirmoy.avro@gmail.com>
2022-03-09 23:58:57 +0600

    CF 379A

---
+++ Codeforces/Solutions/CF - 379A - New Year Candles.c
@@ -0,0 +1,12 @@
+//149018065 	Mar/09/2022 23:58UTC+6 	ItsTHEAvro 	379A - New Year Candles 	GNU C11 	Accepted 	15 ms 	0 KB
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d",(a*b-1)/(b-1));
+
+    return 0;
+}
